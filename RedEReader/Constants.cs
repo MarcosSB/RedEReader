7fe8ccf3b164a2e727046f98fc96e39a973866cc
MarcosSanchez <civilon46@hotmail.com>
2021-01-07 14:38:55 +0100

    Add project files.

---
+++ RedEReader/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedEReader
+{
+    public class Constants
+    {
+        public string RedEUrl = "apidatos.ree.es";
+    }
+}
---
+++ RedEReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RedEReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
